6c3bf75fb24bccd32365755a667228ab8948d512
Younes <101892216+younes-47@users.noreply.github.com>
2024-03-27 01:46:10 +0000

    added blur effect to header

--- app/components/Styled/StyledHeader.js
+++ app/components/Styled/StyledHeader.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
+  position: sticky;
+  z-index: 100;
+  top: 0;
+  width: 100%;
   display: flex;
-  padding: 1.5em 3em;
+  padding: 1em 3em;
   align-items: center;
   justify-content: space-between;
+  background: rgba(255, 255, 255, 0.55);
+  -webkit-backdrop-filter: blur(20px);
+  backdrop-filter: blur(20px);
 `;
