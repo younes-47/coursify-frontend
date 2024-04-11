e1fbbc4b22b3f81fa06a2f3c539722fa55abe0ec
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-11 17:07:54 +0000

    Add useAxiosPrivate and useRefreshToken hooks

---
+++ app/utils/custom/hooks/useAxiosPrivate.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import axiosPrivate from '../api/axios';
+import useRefreshToken from './useRefreshToken';
+import useAuth from './useAuth';
+
+// This hook is used to make private requests to the server.
+// It attaches the access token to the request headers.
+// It also uses the useRefreshToken hook to refresh the access token if it is expired.
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers.Authorization) {
+          // eslint-disable-next-line no-param-reassign
+          config.headers.Authorization = `Bearer ${auth?.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error),
+    );
+
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 403 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+          const newAccessToken = await refresh();
+          prevRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth, refresh]);
+
+  return axiosPrivate;
+};
+
+export default useAxiosPrivate;
---
+++ app/utils/custom/hooks/useRefreshToken.js
@@ -0,0 +1,19 @@
+import { axiosCredentials } from '../api/axios';
+import useAuth from './useAuth';
+
+// This hook is used to refresh the access token if it is expired.
+// It returns a function that can be called to refresh the token.
+// It also updates the access token in the auth context.
+
+const useRefreshToken = () => {
+  const { setAuth } = useAuth();
+
+  const refresh = async () => {
+    const response = await axiosCredentials.post('/Auth/refresh');
+    setAuth((prev) => ({ ...prev, accessToken: response.data.accessToken }));
+    return response.data.accessToken;
+  };
+  return refresh;
+};
+
+export default useRefreshToken;
