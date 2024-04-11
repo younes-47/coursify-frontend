4296a857c2f593ad0c31f91826bf7562e5c0b990
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-11 17:08:01 +0000

    Add axios.js file for custom API configuration

---
+++ app/utils/custom/api/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+// const BASE_URL = 'https://localhost:7142/api/';
+const BASE_URL = 'http://localhost:5041/api/';
+
+// This axios instance is used with public routes (no interceptors nor cookies)
+export const axiosPublic = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+// This axios instance is used with private routes (with interceptors and cookies)
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+// This axios instance is used to set/send the refresh token (no interceptors but with cookies)
+export const axiosCredentials = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+export default axiosPrivate;
