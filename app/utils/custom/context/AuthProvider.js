53c0779da6b77f42232de0cd473598d8a538dfde
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-20 18:27:04 +0100

    Update AuthProvider to include persist and userInfo states

--- app/utils/custom/context/AuthProvider.js
+++ app/utils/custom/context/AuthProvider.js
@@ -5,10 +5,20 @@ const AuthContext = createContext();
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
+  const [persist, setPersist] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem('persist')) || false;
+    } catch (error) {
+      return false;
+    }
+  });
 
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider
+      value={{ auth, setAuth, persist, setPersist, userInfo, setUserInfo }}
+    >
       {children}
     </AuthContext.Provider>
   );
