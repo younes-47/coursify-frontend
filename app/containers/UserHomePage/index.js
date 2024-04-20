487ba5d2fae25930b2f69d6760c9e7948a0fb8a9
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-20 18:28:47 +0100

    Refactor UserHomePage component to remove unused code and update content

--- app/containers/UserHomePage/index.js
+++ app/containers/UserHomePage/index.js
@@ -4,20 +4,14 @@
  *
  */
 
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
+
 import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
-import makeSelectUserHomePage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import useAxiosPrivate from '../../utils/custom/hooks/useAxiosPrivate';
-import { StyledButton } from '../../components/Styled/StyledButton';
 
 const mapStateToProps = createStructuredSelector({});
 
@@ -25,30 +19,7 @@ export function UserHomePage() {
   useInjectReducer({ key: 'userHomePage', reducer });
   useInjectSaga({ key: 'userHomePage', saga });
 
-  const [number, setNumber] = useState(null);
-
-  const axios = useAxiosPrivate();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleOnButtonClick = async () => {
-    try {
-      const response = await axios.get('/User/test');
-      setNumber(response.data);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-      navigate('/login', { state: { from: location }, replace: true });
-    }
-  };
-
-  return (
-    <>
-      <h1>Hello there i am a user</h1>
-      <button onClick={() => handleOnButtonClick()}>Show number</button>
-      <h2>{number?.number}</h2>
-    </>
-  );
+  return <h1>This is Home Page</h1>;
 }
 
 UserHomePage.propTypes = {};
