c940bd0216bfab6fb1b1cf2cf151eea12935cc5e
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-20 18:21:59 +0100

    Added snackbar and loading indicator with app logo

--- app/components/LoadingIndicator/index.js
+++ app/components/LoadingIndicator/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import 'animate.css';
 
 import CircularProgress from '@mui/joy/CircularProgress';
 import Typography from '@mui/joy/Typography';
 import Wrapper from './Wrapper';
+import brainLogo from '../../images/brain-icon.png';
 
 const LoadingIndicator = () => (
   <Wrapper>
-    <CircularProgress color="neutral" size="md" variant="soft" />
+    {/* <CircularProgress color="neutral" size="md" variant="soft" /> */}
+    <img
+      height="35px"
+      width="35px"
+      src={brainLogo}
+      alt="coursify-logo-brain-variant"
+      className="animate__heartBeat"
+    />
     <Typography level="title-lg">En cours...</Typography>
   </Wrapper>
 );
---
+++ app/components/MySnackbar/index.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * MySnackbar
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Snackbar from '@mui/joy/Snackbar';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DangerousSharpIcon from '@mui/icons-material/DangerousSharp';
+import InfoSharpIcon from '@mui/icons-material/InfoSharp';
+import CloseSharpIcon from '@mui/icons-material/CloseSharp';
+import IconButton from '@mui/joy/IconButton';
+// import styled from 'styled-components';
+
+function MySnackbar({ message, open, setOpen, color }) {
+  const startIcon = () => {
+    if (color === 'success') {
+      return <CheckCircleIcon />;
+    }
+    if (color === 'error') {
+      return <DangerousSharpIcon />;
+    }
+    return <InfoSharpIcon />;
+  };
+  return (
+    <Snackbar
+      variant="solid"
+      open={open}
+      autoHideDuration={3000}
+      onClose={() => setOpen(false)}
+      message={message}
+      color={color}
+      startDecorator={startIcon()}
+      endDecorator={
+        <IconButton
+          variant="solid"
+          onClick={() => setOpen(false)}
+          size="sm"
+          color={color}
+        >
+          <CloseSharpIcon />
+        </IconButton>
+      }
+    >
+      {message}
+    </Snackbar>
+  );
+}
+
+MySnackbar.propTypes = {
+  message: PropTypes.string,
+  open: PropTypes.bool,
+  setOpen: PropTypes.func,
+  color: PropTypes.string,
+};
+
+export default MySnackbar;
