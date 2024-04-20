6502b271d69e2d5c5ddb14efe4f974487a883f34
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-20 18:27:54 +0100

    Refactor string manipulation functions and remove unused code

--- app/utils/custom/stringManipulation.js
+++ app/utils/custom/stringManipulation.js
@@ -1,27 +1,11 @@
-const camelToKebab = (str) =>
-  str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-const kebabToCamel = (str) =>
-  str
-    .replace(/-([a-z])/g, (match, letter) => letter.toUpperCase())
-    .replace(/^-/, '');
-const camelToTitle = (str) =>
-  str
-    .replace(/([a-z])([A-Z])/g, '$1 $2')
-    .replace(/([A-Z])([A-Z])([a-z])/g, '$1 $2$3')
-    .replace(/\b\w/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1));
-const DateTimeFormater = (DT) => {
-  // Changes date time format from (yyyy-mm-ddThh:mm:ssz) to yyyy/mm/dd hh:mm
-  const date = new Date(DT);
-  const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(
-    date.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, '0')}/${date.getFullYear()}  `;
-  const formattedTime = `${date.getHours().toString().padStart(2, '0')}:${date
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')}`;
-  return `${formattedDate} ${formattedTime}`;
+export const formatNumberToDate = (number) => {
+  // Converts a number to a date string
+  const date = new Date(number);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+  const year = date.getFullYear();
+  console.log(`${day}-${month}-${year}`);
+  return `${day}-${month}-${year}`;
 };
 
 export const formatName = (name) => {
@@ -31,5 +15,3 @@ export const formatName = (name) => {
     noSpaces.charAt(0).toUpperCase() + noSpaces.slice(1).toLowerCase();
   return capitalized.slice(0, 40);
 };
-
-export { camelToKebab, kebabToCamel, camelToTitle, DateTimeFormater };
