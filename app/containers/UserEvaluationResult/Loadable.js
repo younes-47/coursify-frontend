48f0b84b577a47f6637b83b1da627e9b82a62b98
Younes <101892216+younes-47@users.noreply.github.com>
2024-05-07 12:46:11 +0100

    Added loading indicator

--- app/containers/UserEvaluationResult/Loadable.js
+++ app/containers/UserEvaluationResult/Loadable.js
@@ -4,6 +4,10 @@
  *
  */
 
+import React from 'react';
 import loadable from 'utils/loadable';
+import LoadingIndicator from '../../components/LoadingIndicator';
 
-export default loadable(() => import('./index'));
+export default loadable(() => import('./index'), {
+  fallback: <LoadingIndicator />,
+});
