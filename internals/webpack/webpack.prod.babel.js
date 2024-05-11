c3ce4df1635ea27f5c5fc737651bb91d798bcc02
Younes <101892216+younes-47@users.noreply.github.com>
2024-05-11 12:41:10 +0100

    updated webpakck prod configuration

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -2,8 +2,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const OfflinePlugin = require('offline-plugin');
-const { HashedModuleIdsPlugin } = require('webpack');
+// const OfflinePlugin = require('offline-plugin');
+// const { HashedModuleIdsPlugin } = require('webpack');
+const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
@@ -87,27 +88,27 @@ module.exports = require('./webpack.base.babel')({
 
     // Put it in the end to capture all the HtmlWebpackPlugin's
     // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
-    new OfflinePlugin({
-      relativePaths: false,
-      publicPath: '/',
-      appShell: '/',
+    // new OfflinePlugin({
+    //   relativePaths: false,
+    //   publicPath: '/',
+    //   appShell: '/',
 
-      // No need to cache .htaccess. See http://mxs.is/googmp,
-      // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
+    //   // No need to cache .htaccess. See http://mxs.is/googmp,
+    //   // this is applied before any match in `caches` section
+    //   excludes: ['.htaccess'],
 
-      caches: {
-        main: [':rest:'],
+    //   caches: {
+    //     main: [':rest:'],
 
-        // All chunks marked as `additional`, loaded after main section
-        // and do not prevent SW to install. Change to `optional` if
-        // do not want them to be preloaded at all (cached only when first loaded)
-        additional: ['*.chunk.js'],
-      },
+    //     // All chunks marked as `additional`, loaded after main section
+    //     // and do not prevent SW to install. Change to `optional` if
+    //     // do not want them to be preloaded at all (cached only when first loaded)
+    //     additional: ['*.chunk.js'],
+    //   },
 
-      // Removes warning for about `additional` section usage
-      safeToUseOptionalCaches: true,
-    }),
+    //   // Removes warning for about `additional` section usage
+    //   safeToUseOptionalCaches: true,
+    // }),
 
     new CompressionPlugin({
       algorithm: 'gzip',
@@ -117,20 +118,20 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Coursify - E-Learning Platform',
+      short_name: 'Coursify',
+      description: 'Online learning platform for everyone',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/brain-icon.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/brain-icon.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
@@ -142,6 +143,11 @@ module.exports = require('./webpack.base.babel')({
     //   hashDigest: 'hex',
     //   hashDigestLength: 20,
     // }),
+    new webpack.ids.HashedModuleIdsPlugin({
+      hashFunction: 'sha256',
+      hashDigest: 'hex',
+      hashDigestLength: 20,
+    }),
   ],
 
   performance: {
