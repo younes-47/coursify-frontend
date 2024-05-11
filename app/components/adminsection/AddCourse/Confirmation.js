a75d19b42b0f850b8f18e8557516881ba516533e
Younes <101892216+younes-47@users.noreply.github.com>
2024-05-11 23:16:24 +0100

    Added an overlay to indicate that the action may take a while

--- app/components/adminsection/AddCourse/Confirmation.js
+++ app/components/adminsection/AddCourse/Confirmation.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Sheet from '@mui/joy/Sheet';
 import Typography from '@mui/joy/Typography';
 import PropTypes from 'prop-types';
+import CircularProgress from '@mui/joy/CircularProgress';
 import { useDispatch } from 'react-redux';
+import { Box } from '@mui/joy';
 import { StyledButton } from '../../Styled/StyledButton';
 import ErrorMessage from '../../../containers/AdminCoursesAdd/ErrorMessage';
+import LoadingIndicator from '../../LoadingIndicator';
 
 const Confirmation = ({ adding, add, data, error }) => {
   const dispatch = useDispatch();
@@ -16,29 +19,53 @@ const Confirmation = ({ adding, add, data, error }) => {
         my: 4, // margin top & bottom
         py: 3, // padding top & bottom
         px: 2, // padding left & right
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
         borderRadius: 'sm',
-        filter: adding ? 'blur(2px)' : 'none',
       }}
       variant="outlined"
     >
-      <Typography level="title-lg">Confirmation</Typography>
-      <Typography level="title-md">
-        Veuillez vérifier toutes les informations avant de continuer. En
-        cliquant sur ce bouton, vous allez ajouter la course, qui sera
-        disponible pour les utilisateurs afin de s&apos;y inscrire. Êtes-vous
-        sûr de vouloir continuer ?
-      </Typography>
-      {error && <ErrorMessage error={error} />}
-      <StyledButton
-        color="darkOrange"
-        onClick={() => dispatch(add(data))}
-        disabled={adding}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          filter: adding ? 'blur(2px)' : 'none',
+        }}
       >
-        Ajouter course au platforme
-      </StyledButton>
+        <Typography level="title-lg">Confirmation</Typography>
+        <Typography level="title-md">
+          Veuillez vérifier toutes les informations avant de continuer. En
+          cliquant sur ce bouton, vous allez ajouter la course, qui sera
+          disponible pour les utilisateurs afin de s&apos;y inscrire. Êtes-vous
+          sûr de vouloir continuer ?
+        </Typography>
+        {error && <ErrorMessage error={error} />}
+        <StyledButton
+          color="darkOrange"
+          onClick={() => dispatch(add(data))}
+          disabled={adding || data?.courseInfo?.courseCategoryId === null}
+        >
+          Ajouter course au platforme
+        </StyledButton>
+      </Box>
+
+      {adding && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 100,
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+          }}
+        >
+          <LoadingIndicator color="secondary" />
+        </div>
+      )}
     </Sheet>
   );
 };
